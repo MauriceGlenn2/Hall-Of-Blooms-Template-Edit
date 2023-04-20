5f2acf735a96ba6b2fdddd59351e6db5de7e55f8
Maurice <mauriceglenn2@gmail.com>
2023-04-20 18:33:36 -0500

    New font added

--- hallofblooms/scrolling/Scrollingimg.js
+++ hallofblooms/scrolling/Scrollingimg.js
@@ -3,9 +3,23 @@ const image = document.querySelector(".scroll__image--container img");
 const leftArrow = document.querySelector(".arrow-left");
 const rightArrow = document.querySelector(".arrow-right");
 const images = [
-  "./img/ef2.jpg",
-  "./img/ef3.jpg",
-  "./img/ef4.jpg",
+  "./img/galleryphotos/IMG_0158.jpg",
+  "./img/galleryphotos/IMG_0185.jpg",
+  "./img/galleryphotos/IMG_0193_jpg.jpg",
+  "./img/galleryphotos/IMG_0203.jpg",
+  "./img/galleryphotos/IMG_0210.jpg",
+  "./img/galleryphotos/IMG_0597.jpg",
+  "./img/galleryphotos/IMG_0625.jpg",
+  "./img/galleryphotos/IMG_1137.jpg",
+  "./img/galleryphotos/IMG_1171.jpg",
+  "./img/galleryphotos/IMG_1434.jpg",
+  "./img/galleryphotos/IMG_1952.jpg",
+  "./img/galleryphotos/IMG_7396.jpg",
+  "./img/galleryphotos/IMG_7410.jpeg",
+  "./img/galleryphotos/IMG_7411.jpg",
+  "./img/galleryphotos/IMG_8374.jpg",
+  "./img/galleryphotos/IMG_8450_jpg.jpg",
+  "./img/galleryphotos/IMG_9423_jpg.jpg",
 ];
 let index = 0;
 
